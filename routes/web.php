97bae1d3ae7619e7d422a11185cf0d7d83c919ae
ferdyhape <ferdyhahan5@gmail.com>
2023-01-09 02:13:47 +0700

    setup route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/table', [DashboardController::class, 'table'])->name('tabledashboard');
+    Route::get('/utilities-color', [DashboardController::class, 'utilities_color']);
+    Route::get('/utilities-border', [DashboardController::class, 'utilities_border']);
+    Route::get('/utilities-animation', [DashboardController::class, 'utilities_animation']);
+    Route::get('/utilities-other', [DashboardController::class, 'utilities_other']);
+    Route::get('/buttons', [DashboardController::class, 'buttons']);
+    Route::get('/cards', [DashboardController::class, 'cards']);
+    Route::get('/charts', [DashboardController::class, 'charts']);
+    Route::get('/blank', [DashboardController::class, 'blank'])->name('blank');
+    Route::get('/404', [DashboardController::class, 'error_404'])->name('404');
+    Route::get('/login', [DashboardController::class, 'login'])->name('dashboardlogin');
+    Route::get('/register', [DashboardController::class, 'register'])->name('dashboardregister');
+    Route::get('/forgot-password', [DashboardController::class, 'forgot_password'])->name('dashboardforgorpassword');
 });
